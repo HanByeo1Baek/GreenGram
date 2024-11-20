05212abc33fbcd72b77fd92b897ec348ad17d139
HanByeo1Baek <starts466@gmail.com>
2024-11-20 17:05:15 +0900

    11-20

---
+++ 2024-02-greengram-ver1-master/src/main/resources/query.sql
@@ -0,0 +1,29 @@
+DROP TABLE user;
+CREATE TABLE USER (
+      user_id BIGINT AUTO_INCREMENT PRIMARY KEY
+    , uid VARCHAR(30) UNIQUE NOT NULL
+    , upw VARCHAR(100) NOT NULL
+    , nick_name VARCHAR(30)
+    , pic VARCHAR(70)
+    , created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    , updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP
+);
+
+DROP TABLE feed;
+CREATE TABLE feed (
+      feed_id BIGINT AUTO_INCREMENT PRIMARY KEY
+    , writer_user_id BIGINT NOT NULL
+    , contents VARCHAR(1000)
+    , location VARCHAR(30)
+    , created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    , updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP
+    , FOREIGN KEY (writer_user_id) REFERENCES user(user_id)
+);
+
+CREATE TABLE feed_pics(
+      feed_pic_id BIGINT AUTO_INCREMENT PRIMARY KEY
+    , feed_id BIGINT NOT NULL
+    , pic VARCHAR(70) NOT NULL
+    , created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    , FOREIGN KEY (feed_id) REFERENCES feed(feed_id)
+);
---
+++ src/main/java/com/green/greengramver1/user/model/UserGetRes.java
@@ -0,0 +1,10 @@
+package com.green.greengramver1.user.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserGetRes {
+    private int userId;
+}
